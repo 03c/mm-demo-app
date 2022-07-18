4cf47e3318970b32a33c0d81c07bdaadafe9c662
Chris Child <chris.child@gmail.com>
2022-07-18 09:37:22 +0100

    seed db on start

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,8 @@ rm -f /app/tmp/pids/server.pid
 # run all the migrations
 bundle exec rake db:migrate
 
+# seed the database
+bundle exec rake db:seed
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
